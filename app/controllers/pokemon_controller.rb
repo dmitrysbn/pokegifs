a287109302104d6f1df4cc084758b2753ff01bab
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-19 13:32:09 -0400

    Add gif as json key

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,22 +2,27 @@ class PokemonController < ApplicationController
 
   def show
 
-    response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    body = JSON.parse(response.body)
-    name = body["name"]
-    id = params[:id]
+    pokemon = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    pokemon_body = JSON.parse(pokemon.body)
 
-    types = []
-    body["types"].each do |type|
-      types << type["type"]["name"]
+    pokemon_name = pokemon_body["name"]
+    pokemon_id = params[:id]
+    pokemon_types = []
+    pokemon_body["types"].each do |type|
+      pokemon_types << type["type"]["name"]
     end
+
+    gif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{pokemon_name}&rating=g")
+    gif_body = JSON.parse(gif.body)
+    gif_url = gif_body["data"][0]["url"]
 
     respond_to do |format|
       format.json do
         render json: {
-          name: name,
-          id: id,
-          types: types
+          name: pokemon_name,
+          id: pokemon_id,
+          types: pokemon_types,
+          gif: gif_url
         }
       end
     end
