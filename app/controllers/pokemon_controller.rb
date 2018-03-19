7a06681a2edcb2208ee96ed454a3ea13ba94ff39
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-19 15:00:50 -0400

    Add /team endpoint with random pokemon and gifs

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,6 @@
 class PokemonController < ApplicationController
 
   def show
-
     pokemon = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
     pokemon_body = JSON.parse(pokemon.body)
 
@@ -14,18 +13,44 @@ class PokemonController < ApplicationController
 
     gif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{pokemon_name}&rating=g")
     gif_body = JSON.parse(gif.body)
-    gif_url = gif_body["data"][0]["url"]
+    gif_url = gif_body["data"][rand(gif_body["data"].size)]["url"]
 
-    respond_to do |format|
-      format.json do
-        render json: {
-          name: pokemon_name,
-          id: pokemon_id,
-          types: pokemon_types,
-          gif: gif_url
-        }
-      end
-    end
+    render json: {
+      name: pokemon_name,
+      id: pokemon_id,
+      types: pokemon_types,
+      gif: gif_url
+    }
   end
 
+  def team
+    pokemons = []
+
+    6.times do
+      random_number = 800
+
+      pokemon = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{random_number}/")
+      pokemon_body = JSON.parse(pokemon.body)
+
+      pokemon_name = pokemon_body["name"]
+      pokemon_id = random_number
+      pokemon_types = []
+      pokemon_body["types"].each do |type|
+        pokemon_types << type["type"]["name"]
+      end
+
+      gif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{pokemon_name}&rating=g")
+      gif_body = JSON.parse(gif.body)
+
+      # handles case if no gif for some pokemon
+      if gif_body["data"].any?
+        gif_url = gif_body["data"][rand(gif_body["data"].size)]["url"]
+        pokemons << { name: pokemon_name, id: pokemon_id, types: pokemon_types, gif_url: gif_url }
+      else
+        pokemons << { name: pokemon_name, id: pokemon_id, types: pokemon_types, gif_url: "nil" }
+      end
+    end
+
+    render json: pokemons
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   resources :pokemon, only: [:show]
+  get '/team', to: 'pokemon#team'
 
 end
