df1fa09eefd63f5d04af6c8289f02d0bd595c6c0
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-19 13:08:56 -0400

    Display name, id, types as json response

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,9 +1,24 @@
 class PokemonController < ApplicationController
 
   def show
+
+    response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(response.body)
+    name = body["name"]
+    id = params[:id]
+
+    types = []
+    body["types"].each do |type|
+      types << type["type"]["name"]
+    end
+
     respond_to do |format|
       format.json do
-        render json: { "message": "ok" }
+        render json: {
+          name: "#{name}",
+          id: "#{id}",
+          types: "#{types}"
+        }
       end
     end
   end
