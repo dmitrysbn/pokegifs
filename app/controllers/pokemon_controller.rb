d0a0f4f637d5ab46fc6527040f085f7906376701
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-19 15:20:52 -0400

    Add 404 and 403 errors

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,25 +2,34 @@ class PokemonController < ApplicationController
 
   def show
     pokemon = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    pokemon_body = JSON.parse(pokemon.body)
 
-    pokemon_name = pokemon_body["name"]
-    pokemon_id = params[:id]
-    pokemon_types = []
-    pokemon_body["types"].each do |type|
-      pokemon_types << type["type"]["name"]
+    if pokemon.code == 404
+      render plain: "404: Not Found"
+    else
+      pokemon_body = JSON.parse(pokemon.body)
+
+      pokemon_name = pokemon_body["name"]
+      pokemon_id = params[:id]
+      pokemon_types = []
+      pokemon_body["types"].each do |type|
+        pokemon_types << type["type"]["name"]
+      end
+
+      gif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}5&q=#{pokemon_name}&rating=g")
+      if gif.code == 403
+        render plain: "403: Forbidden."
+      else
+        gif_body = JSON.parse(gif.body)
+        gif_url = gif_body["data"][rand(gif_body["data"].size)]["url"]
+
+        render json: {
+          name: pokemon_name,
+          id: pokemon_id,
+          types: pokemon_types,
+          gif: gif_url
+        }
+      end
     end
-
-    gif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{pokemon_name}&rating=g")
-    gif_body = JSON.parse(gif.body)
-    gif_url = gif_body["data"][rand(gif_body["data"].size)]["url"]
-
-    render json: {
-      name: pokemon_name,
-      id: pokemon_id,
-      types: pokemon_types,
-      gif: gif_url
-    }
   end
 
   def team
